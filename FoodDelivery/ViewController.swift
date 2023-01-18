4ccdce7470aa76b685ae523545f542f34361b25a
Sergio <afirthes@gmail.com>
2023-01-18 19:02:04 +0300

    Initial Commit

---
+++ FoodDelivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by Afir Thes on 18.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
